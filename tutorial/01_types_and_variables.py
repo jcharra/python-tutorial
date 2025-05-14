dd628ffc956011bdb6a5a0d7f7e714b5a0517451
jcharra <johannes.charra@gmail.com>
2025-05-14 14:38:32 +0200

    rename, start 2nd chapter

--- tutorial/01_types_and_variables.py
+++
@@ -1,8 +0,0 @@
-# Hier kann ich schreiben was ich will, der Python-Interpreter wird es nicht
-# interpretieren!
-# Erste Übung:
-print("Das hier sieht man wirklich!")
-print("Hallo\nZeile 2\nZeile 3")
-
-s = 1
-s += "hi"
--- tutorial/04_while.py
+++
@@ -1,5 +0,0 @@
-a = 10
-while a > 0:
-    print("a ist jetzt ", a)
-    a = a - 1
-print("Ende")
